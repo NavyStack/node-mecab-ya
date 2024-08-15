5faefc6cbc70bfc438877eccc09dbc4ae1763c02
NavyStack <navystack@askfront.com>
2024-08-15 12:49:12 +0900

    Update mecab.js

--- mecab.js
+++ mecab.js
@@ -18,28 +18,17 @@ var execMecab = function (text, callback) {
             return callback(err);
         }
         callback(null, stdout);
-    });    
+    });
 };
 
-var parseFunctions = {
-    'pos': function (result, elems) {
-        result.push([elems[0]].concat(elems[1].split(',')[0]));
-        return result;
-    },
-
-    'morphs': function (result, elems) {
-        result.push(elems[0]);
-        return result;
-    },
-
-    'nouns': function (result, elems) {
-        var tag = elems[1].split(',')[0];
-        
-        if (tag === 'NNG' || tag === 'NNP') {
-            result.push(elems[0]);
-        }
-
-        return result;
+var execMecabSync = function (text) {
+    var command = buildCommand(text);
+    try {
+        var stdout = cp.execSync(command);
+        return stdout.toString();
+    } catch (err) {
+        console.error("Error executing mecab:", err);
+        throw err;
     }
 };
 
@@ -61,6 +50,42 @@ var parse = function (text, method, callback) {
     });
 };
 
+var parseSync = function (text, method) {
+    var result = execMecabSync(text);
+
+    return result.split('\n').reduce(function(parsed, line) {
+        var elems = line.split('\t');
+
+        if (elems.length > 1) {
+            return parseFunctions[method](parsed, elems);
+        } else {
+            return parsed;
+        }
+    }, []);
+};
+
+var parseFunctions = {
+    'pos': function (result, elems) {
+        result.push([elems[0]].concat(elems[1].split(',')[0]));
+        return result;
+    },
+
+    'morphs': function (result, elems) {
+        result.push(elems[0]);
+        return result;
+    },
+
+    'nouns': function (result, elems) {
+        var tag = elems[1].split(',')[0];
+
+        if (tag === 'NNG' || tag === 'NNP') {
+            result.push(elems[0]);
+        }
+
+        return result;
+    }
+};
+
 var pos = function (text, callback) {
     parse(text, 'pos', callback);
 };
@@ -73,8 +98,23 @@ var nouns = function (text, callback) {
     parse(text, 'nouns', callback);
 };
 
+var posSync = function (text) {
+    return parseSync(text, 'pos');
+};
+
+var morphsSync = function (text) {
+    return parseSync(text, 'morphs');
+};
+
+var nounsSync = function (text) {
+    return parseSync(text, 'nouns');
+};
+
 module.exports = {
     pos: pos,
     morphs: morphs,
-    nouns: nouns
+    nouns: nouns,
+    posSync: posSync,
+    morphsSync: morphsSync,
+    nounsSync: nounsSync
 };
